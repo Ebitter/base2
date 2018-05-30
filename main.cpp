2dd19dd061d334a8be70baf6d5250ba374ec542c
ebitter <991807771@qq.com>
2018-05-30 09:25:49 +0800

    base1

---
+++ main.cpp
@@ -0,0 +1,13 @@
+﻿#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core/core.hpp>
+#include "stdio.h"
+
+int main(int argc, char *argv[])
+{
+    cv::Mat pic = cv::imread("./pic.jpg");
+
+    cv::imshow("my pic", pic);
+
+    cv::waitKey(0);
+}
